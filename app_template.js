ebfe53a8fe9caa2dfe71beb715f4398dceb9104c
PossiblePack <possible.pack@gmail.com>
2021-01-12 13:56:58 +0700

    Doing

--- app_template.js
+++ app_template.js
@@ -1,1 +1,8 @@
 //your code here!
+let id = '620610814'
+document.getElementById("button-compute").addEventListener("submit", displayModID);
+
+function displayModID(){
+    const input = document.getElementById("input-number").innerHTML
+    console.log('input')
+}
